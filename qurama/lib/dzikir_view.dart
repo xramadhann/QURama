591a2b63f784a8d0cb4e213027640ee6cd3c4ec0
xramadhann <xxdhanz@gmail.com>
2024-06-11 05:42:31 +0700

    feat : create dzikir_view.dart

---
+++ qurama/lib/dzikir_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class dzikir extends StatefulWidget {
+  const dzikir({super.key});
+
+  @override
+  State<dzikir> createState() => _dzikirState();
+}
+
+class _dzikirState extends State<dzikir> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
