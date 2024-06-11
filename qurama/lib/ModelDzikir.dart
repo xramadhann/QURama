526f403145640263183dddcdbc276c20e495e0db
xramadhann <xxdhanz@gmail.com>
2024-06-12 03:29:05 +0700

    feat : models data dzikir

---
+++ qurama/lib/ModelDzikir.dart
@@ -0,0 +1,19 @@
+
+class DataDzikir {
+  final String arab;
+  final String latin;
+  final String arti;
+}
+const DataDzikir({
+  required this.arab,
+  required this.latin,
+  required this.arti,
+});
+}
+const allDataDzikir = [
+  DataDzikir(
+    arab: 'بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ',
+    latin: 'Bismillaahirrahmaanirrahiim',
+    arti: 'Segala puji bagi Allah yang telah memberikan rahmat dan berkah',
+  )
+];
