457cc3f08254d44a35f14c3459c325421655dc10
xramadhann <xxdhanz@gmail.com>
2024-06-12 01:39:40 +0700

    Feat: create KiblatView.dart

---
+++ qurama/lib/KiblatView.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class Kiblat extends StatefulWidget {
+  const Kiblat({super.key});
+
+  @override
+  State<Kiblat> createState() => _KiblatState();
+}
+
+class _KiblatState extends State<Kiblat> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
