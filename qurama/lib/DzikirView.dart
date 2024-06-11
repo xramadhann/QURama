dcd15b0384c4d4c12ecfc93f4d0b2df9f6cf2827
xramadhann <xxdhanz@gmail.com>
2024-06-12 05:21:49 +0700

    style : styleing dzikir text

--- qurama/lib/DzikirView.dart
+++ qurama/lib/DzikirView.dart
@@ -35,62 +35,76 @@ class _dzikirState extends State<dzikir> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(top: 50),
-              child: const Text(
-                "dzikir",
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              height: 300,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                padding: const EdgeInsetsDirectional.only(start: 20, end: 20),
-                itemCount: allDataDzikir.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: const EdgeInsets.only(left: 5, right: 5),
-                    padding: EdgeInsets.all(20),
-                    width: 380,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color.fromARGB(255, 0, 66, 88),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          allDataDzikir[index].arab,
-                          style: GoogleFonts.alexandria().copyWith(
-                            fontSize: 19,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.center,
+            Stack(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(
+                      top: 70, left: 165, right: 155, bottom: 100),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: const Radius.circular(20)),
+                    color: Color.fromARGB(255, 0, 66, 88),
+                  ),
+                  height: 410,
+                  width: 500,
+                  child: const Text(
+                    "dzikir",
+                    style: TextStyle(fontSize: 40, color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 120),
+                SizedBox(
+                  height: 380,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    padding:
+                        const EdgeInsetsDirectional.only(start: 20, end: 20),
+                    itemCount: allDataDzikir.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.only(left: 15, top: 130),
+                        padding: const EdgeInsets.all(20),
+                        width: 380,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: const Color.fromARGB(255, 0, 66, 88),
                         ),
-                        const SizedBox(height: 20),
-                        Text(
-                          allDataDzikir[index].latin,
-                          style: const TextStyle(
-                              fontSize: 14, color: Colors.white),
-                          textAlign: TextAlign.center,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              allDataDzikir[index].arab,
+                              style: GoogleFonts.alexandria().copyWith(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              allDataDzikir[index].latin,
+                              style: const TextStyle(
+                                  fontSize: 18, color: Colors.white),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              allDataDzikir[index].arti,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
                         ),
-                        const SizedBox(height: 10),
-                        Text(
-                          allDataDzikir[index].arti,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: Colors.white,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
             const SizedBox(height: 20),
             Container(
--- qurama/lib/ModelDzikir.dart
+++ qurama/lib/ModelDzikir.dart
@@ -14,19 +14,30 @@ class DataDzikir {
 
 const allDataDzikir = [
   DataDzikir(
-    arab:
-        "أَسْتَغْفِرُ اللهَ (٣×) اللَّهُمَّ أَنْتَ السَّلامُ، وَمِنْكَ السَّلَامُ، تَبَارَكْتَ يَا ذَا الْجَلَالِ وَالْإِكْرَامِ",
-    latin:
-        "Astaghfirullah (3x) Allahumma anta assalaam, wa minka assalaam, tabaarakta yaa dzal jalaali wal ikraam",
-    arti:
-        "Aku memohon ampun kepada Allah, (3x). Ya Allah, Engkau Mahasejahtera, dan dari-Mu kesejahteraan, Mahasuci Engkau, wahai Rabb Pemilik keagungan dan kemuliaan.",
+    arab: "ٱللَّٰهُ أَكْبَرُ",
+    latin: "allahu 'akbar",
+    arti: "Allah Maha Besar",
+  ),
+  DataDzikir(
+    arab: "سُبْحَانَ ٱللَّٰهِ",
+    latin: "subhan allah",
+    arti: "Maha suci Allah",
+  ),
+  DataDzikir(
+    arab: "ٱلْحَمْدُ لِلَّٰهِ",
+    latin: "alhamdlilah",
+    arti: "Segala puji bagi Allah",
   ),
   DataDzikir(
     arab:
-        "لا إِلَهَ إِلَّا اللهُ وَحْدَهُ لَا شَرِيكَ لَهُ ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيرُ، اللَّهُمَّ لَا مَانِعَ لِمَا أَعْطَيْتَ، وَلَا مُعْطِيَ لِمَا مَنَعْت، وَلَا يَنْفَعُ ذَا الْجَدِ مِنْكَ الْجَدُّ",
-    latin:
-        "Laa ilaaha illa Allahu wahdahu laa syariika lahu, lahul mulku wa lahul hamdu wa huwa 'ala kulli syai'in qadiir, Allahumma laa maani'a limaa a'thaita wa laa mu'thiya limaa mana'ta wa laa dzal jaddi minka aljaddu",
+        "سُبْحَانَ اللَّهِ وَالْحَمْدُ لِلَّهِ وَلاَ إِلَهَ إِلاَّ اللَّهُ وَاللَّهُ أَكْبَرُ",
+    latin: "subhan allah walhamd lilah wala 'iilah 'iilaa allah wallah 'akbar",
     arti:
-        "Tidak ada ilah yang berhak diibadahi dengan benar melainkan hanya Allah Yang Mahaesa, tidak ada sekutu bagi-Nya. Bagi-Nya segala ke- rajaan dan bagi-Nya segala pujian. Dia Mahakuasa atas segala sesuatu. Ya Allah, tidak ada yang men- cegah apa yang Engkau beri dan tidak ada yang memberi apa yang Engkau cegah. Tidak berguna kekayaan dan kemuliaan itu bagi pemiliknya dari (siksa)-Mu.",
+        "Maha Suci Allah, puji bagi Allah, tidak ada Tuhan selain Allah, dan Allah Maha Besar",
+  ),
+  DataDzikir(
+    arab: "سُبْحَانَ اللَّهِ وَبِحَمْدِهِ سُبْحَانَ اللَّه الْعَظِيم",
+    latin: "subhan allah wabihamdih subhan allah aleazim",
+    arti: "Maha Suci Allah dan Segala puji hanya bagi-Nya",
   ),
 ];
